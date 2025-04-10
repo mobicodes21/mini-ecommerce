17dc9898a3933b23fa254605a54b33cd203aa40c
Mobina Alomohammadi <mobinaalimohammadi40@gmail.com>
2025-04-10 20:21:29 +0300

    remove unnecessary files

--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import cartReducer from './cart/cartSlice';
-import { configureStore } from "@reduxjs/toolkit";
-
-export const store = configureStore({
-    reducer:{
-        cart: cartReducer
-    }
-})
-export default store;
