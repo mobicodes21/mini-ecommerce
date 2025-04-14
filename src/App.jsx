f6321cd9ca4e80a07046baefbf934dfe3f9826cf
Mobina Alomohammadi <mobinaalimohammadi40@gmail.com>
2025-04-14 16:01:35 +0300

    add react-router and basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+
+import Cart from "./components/Cart"
 import Home from "./pages/Home"
+
 function App() {
   return (
-    <>
-    <Home/>
-    </>
+    <Router>
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path="/cart" element={<\Cart/>}/>
+      </Routes> 
+    </Router>
   )
 }
 
