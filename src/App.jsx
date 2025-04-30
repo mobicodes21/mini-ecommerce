4e8f825b07a526f763ba79a8cca4ff45c368aa71
Mobina Alomohammadi <mobinaalimohammadi40@gmail.com>
2025-04-30 21:23:53 +0300

    set up components in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 
+import Banner from "./pages/Banner"
 import Cart from "./components/Cart"
+import Footer from './components/Footer'
 import Home from "./pages/Home"
+import LoginPage from "./pages/LoginPage"
+import Navbar from "./pages/Navbar"
+import ProductDetails from "./components/ProductDetails"
+import SignUpPage from './pages/SignUp'
 
 function App() {
   return (
-    <Router>
+
+
+<Router>
+      <Banner/>
+    <Navbar/>
      <Routes>
       <Route path='/' element={<Home/>}/>
-      <Route path="/cart" element={<\Cart/>}/>
+      <Route path="/cart" element={<Cart/>}/>
+      <Route path="/login" element={<LoginPage/>}/>
+      <Route path="/signup" element={<SignUpPage/>}/>
+
+      <Route path="/product/:id" element={<ProductDetails/>}/>
       </Routes> 
+      <Footer/>
     </Router>
+
   )
 }
 
