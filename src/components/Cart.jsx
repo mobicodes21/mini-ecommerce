129798bf75927ee3a34a3ac22dab4bcd6ac91714
Mobina Alomohammadi <mobinaalimohammadi40@gmail.com>
2025-04-30 21:28:32 +0300

    create Cart.jsx and set up

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,13 +1,14 @@
+import { Box, Button, Card, CardContent, Typography } from '@mui/material';
+import { clearCart, removeItemFromCart, selectTotalPrice } from '../redux/cart/cartSlice';
 import { useDispatch, useSelector } from 'react-redux'
 
 import React from 'react'
-import { removeItemFromCart } from '../redux/cart/cartSlice';
 
 export default function Cart() {
     const dispatch = useDispatch();
     const cartItems =  useSelector((state) =>
     state.cart.items);
-
+    const totalPrice = useSelector(selectTotalPrice);
     // const handleAddItem = (item)=>{
     //     dispatch(addItemToCart(item));
     // };
@@ -15,21 +16,36 @@ export default function Cart() {
     const handleRemoveItem = (id)=>{
         dispatch(removeItemFromCart(id));
     };
+    const handleClearCart = ()=>{
+        dispatch(clearCart())
+    }
   return (
-    <div>
-        <h2>Your Cart</h2>
+    <Box sx={{padding: 2}}>
+        <Typography variant='h4' gutterBottom>سبد خرید</Typography>
         {cartItems.length === 0 ? (
-            <p>Cart is empty.</p>
+            <Typography variant='h6'>سبد خرید شما خالی است</Typography>
         ) : (
             <ul>
                 {cartItems.map((item)=> (
-                <li key={item.id}>
-                    {item.name} - {item.price}$
-                    <button onClick={()=>handleRemoveItem(item.id)}>Remove</button>
-                </li>    
+                <>
+                <Card key={item.id}>
+                    <CardContent>
+                        <Typography variant='h6'>{item.name}</Typography>
+                        <Typography color='text.secondary' variant='h6'>${item.price}</Typography>
+                        <Typography variant='h6'>{item.quantity}</Typography>
+                        {console.log('price: ', item.price, 'quantity: ',item.quantity)}
+                        <Typography>{(parseFloat(item.price) * parseInt(item.quantity)).toLocaleString()}</Typography>
+                    </CardContent>
+                    <Button variant='outlined' color='error' onClick={()=>handleRemoveItem(item.id)}>Remove</Button>
+                </Card>
+                <div>
+                    <Typography variant='h6'>{totalPrice.toLocaleString()}</Typography>
+                    <Button onClick={handleClearCart}>Clear your cart</Button>
+                </div>
+                </>
                 ))}
             </ul>
         )}
-    </div>
+    </Box>
   )
 }
