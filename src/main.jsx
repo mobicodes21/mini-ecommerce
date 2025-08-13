e9caf65ee2c5d66e3c8f893ccc81f5b6922b1f19
Mobina Alomohammadi <mobinaalimohammadi40@gmail.com>
2025-08-13 19:49:11 +0300

    fix: remove redundant import in entry file

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,21 @@
 import './index.css'
+import './App'
 
 import App from './App.jsx'
+import { CssBaseline } from '@mui/material'
 import { Provider } from 'react-redux'
+import { ThemeProvider } from '@emotion/react'
 import { createRoot } from 'react-dom/client'
 import store from './redux/store.jsx'
+import theme from './theme/index.js'
 
 createRoot(document.getElementById('root')).render(
 
-    <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Provider store={store}>
       <App />
     </Provider>
+    </ThemeProvider>
 
 )
