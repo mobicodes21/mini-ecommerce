6cfcb5239b81360dfa6694721baebd62176f3852
Mobina Alomohammadi <mobinaalimohammadi40@gmail.com>
2025-08-13 19:50:11 +0300

    feat(store): configure Redux store with cart, products, and popularProducts slices

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,9 +1,13 @@
 import cartReducer from './cart/cartSlice';
 import { configureStore } from "@reduxjs/toolkit";
+import popularProductsReducer from './popularProducts/popularProductsSlice'
+import productsReducer from './products/productsSlice'
 
 export const store = configureStore({
     reducer:{
-        cart: cartReducer
+        cart: cartReducer,
+        products: productsReducer,
+        popularProducts: popularProductsReducer
     }
 })
 export default store;
